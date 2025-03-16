9d5c53f07cc3a19f1d362be60701415c11dd077d
GustavoSantos <gsf34062@gmail.com>
2025-03-15 21:48:42 -0300

    H2 database, test profile, JPA

--- src/main/java/com/codefylab/learningspringboot/entities/User.java
+++ src/main/java/com/codefylab/learningspringboot/entities/User.java
@@ -1,19 +1,24 @@
 package com.codefylab.learningspringboot.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.Locale;
 import java.util.Objects;
 
+@Entity
+@Table (name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    public Long id;
-    public String name;
-    public String email;
-    public String phone;
-    public String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
 
     public User() {}
 
