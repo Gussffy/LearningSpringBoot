2dfcb0e01e9534db4c218cf7b6004634bc20b7ec
GustavoSantos <gsf34062@gmail.com>
2025-07-11 21:03:43 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/codefylab/learningspringboot/entities/Product.java
+++ src/main/java/com/codefylab/learningspringboot/entities/Product.java
@@ -1,5 +1,6 @@
 package com.codefylab.learningspringboot.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -24,9 +25,13 @@ public class Product implements Serializable {
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn (name = "product_id"),inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product() {
 
     }
+
     public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
         this.name = name;
@@ -67,6 +72,15 @@ public class Product implements Serializable {
    }
    public Set<Category> getCategories() {
         return categories;
+   }
+
+   @JsonIgnore
+   public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+       for (OrderItem x : items) {
+           set.add(x.getOrder());
+       }
+       return set;
    }
 
     @Override
