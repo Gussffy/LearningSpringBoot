6d646a21ba40a12aee5e3e17b7d4dd69ca4cba0e
GustavoSantos <gsf34062@gmail.com>
2025-07-12 17:46:56 -0300

    Exception handling - delete

--- src/main/java/com/codefylab/learningspringboot/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/codefylab/learningspringboot/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.codefylab.learningspringboot.resources.exceptions;
 
+import com.codefylab.learningspringboot.services.exceptions.DatabaseException;
 import com.codefylab.learningspringboot.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,13 @@ public class ResourceExceptionHandler {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
 
+        StardadError err = new StardadError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StardadError>database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StardadError err = new StardadError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
